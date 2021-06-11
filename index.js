fc55feb8606f65f6c2700675cba73e0ffc0ac80f
Giovani Pires <giovani.pires@gmail.com>
2021-06-10 22:01:50 -0300

    JS com O.O, primeiros passos da aula 1.

---
+++ index.js
@@ -0,0 +1,12 @@
+const cliente1Nome = `Giovani`;
+const cliente1CPF = `05405130090`;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = `Marianna`;
+const cliente2CPF = `05405130099`;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+console.log(cliente1Nome);
+console.log(cliente2Nome);
