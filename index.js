d156d9a36ad779046df9f0373d3c810a2ca7f8a8
Giovai Pires <giovani.pires@gmail.com>
2021-06-13 22:52:59 -0300

    JS com O.O, criando uma nova classe.

--- index.js
+++ index.js
@@ -2,22 +2,35 @@
 class Cliente{
 	nome;
 	cpf;
-	agencia;
-	saldo;
 	rg;
 };
+
+class Conta {
+	agencia;
+	conta
+	saldo;
+}
 
 const cliente1 = new Cliente();
 cliente1.nome = `Giovani`;
 cliente1.cpf = `05405130090`;
-cliente1.agencia = `1001`;
-cliente1.saldo = 0;
-cliente1.rg = 0127009000;
+cliente1.rg = `0127009000`;
+
+const contaCliente1 = new Conta();
+contaCliente1.agencia = `1001`;
+contaCliente1.conta = `001-x`;
+contaCliente1.saldo = 0;
+
 
 const cliente2 = new Cliente();
 cliente2.nome = `Marianna`;
 cliente2.cpf = `05405130099`;
-cliente2.agencia = `1001`;
-cliente2.saldo = 0;
+cliente2.rg = `0127879000`;
 
-console.log(cliente1, cliente2);
+const contaCliente2 = new Conta();
+contaCliente2.agencia = `1001`;
+contaCliente2.conta = `002-x`;
+contaCliente2.saldo = 0;
+
+console.log(cliente1, contaCliente1);
+console.log(cliente2, contaCliente2);
